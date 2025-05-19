0610b7cfbfb953ba374657c0373defaf73c976ea
Richard Gilbert <gilbert67@gmail.com>
2025-05-18 21:02:53 -0700

    watching component twig as well.

--- web/themes/custom/however/browsersync.config.js
+++ web/themes/custom/however/browsersync.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   // Your DDEV site URL
   proxy: "https://however-drupal.ddev.site/",
-  
+
   // Watch these files for changes
   files: [
     "dist/css/*.css",
     "dist/js/*.js",
-    "templates/**/*.html.twig"
+    "templates/**/*.html.twig",
+    "components/**/*.html.twig",
   ],
-  
+
   // Auto-open browser window
   open: true,
-  
+
   // Notify on changes
   notify: true,
-  
+
   // Use HTTPS
   https: true,
-  
+
   // Ignore certificate errors
   httpModule: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 };
