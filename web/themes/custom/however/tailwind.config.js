22f38ffeec6d5243222a8cb186743349d3176981
Richard Gilbert <gilbert67@gmail.com>
2025-05-21 15:15:04 -0700

    Theming with Tailwind.

--- web/themes/custom/however/tailwind.config.js
+++ web/themes/custom/however/tailwind.config.js
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./templates/**/*.{html,twig}",
     "./components/**/*.{html,twig}",
     "./src/js/**/*.js",
   ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        "primary-dark": "var(--color-primary-dark)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        yellow: "var(--color-yellow)",
+      },
+    },
+  },
 };
