7418a3409549d815e63f1cde0c1c4a1dff27cdf3
Richard Gilbert <gilbert67@gmail.com>
2026-06-25 11:20:45 -0700

    Reestablishing slider for home page.

--- web/themes/custom/however/src/js/scripts-jquery.js
+++ web/themes/custom/however/src/js/scripts-jquery.js
@@ -1,25 +1,33 @@
-(function ($) {
-  // Slick carousel initialization
-  $(".block-inline-blockimage-carousel > .field--name-field-image").slick();
-  $(
-    ".block-inline-blockcontent-carousel > .field--name-field-content-embed"
-  ).slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      // Add responsive settings if needed...
-    ],
-  });
+(function ($, Drupal, once) {
+  Drupal.behaviors.howeverCarousels = {
+    attach: function (context) {
+      once('however-image-carousel', '.block-inline-blockimage-carousel > .field--name-field-image', context).forEach(function (el) {
+        $(el).slick();
+      });
 
-  // Featherlight gallery
-  $(".field--name-field-journal-page-images-media a").featherlightGallery({
-    gallery: {
-      fadeIn: 300,
-      fadeOut: 300,
-    },
-    openSpeed: 300,
-    closeSpeed: 300,
-  });
-})(jQuery);
+      once('however-content-carousel', '.block-inline-blockcontent-carousel > .field--name-field-content-embed', context).forEach(function (el) {
+        $(el).slick({
+          infinite: true,
+          speed: 300,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        });
+      });
+    }
+  };
+
+  Drupal.behaviors.howeverFeatherlight = {
+    attach: function (context) {
+      once('however-featherlight', '.field--name-field-journal-page-images-media', context).forEach(function (el) {
+        $(el).find('a').featherlightGallery({
+          gallery: {
+            fadeIn: 300,
+            fadeOut: 300,
+          },
+          openSpeed: 300,
+          closeSpeed: 300,
+        });
+      });
+    }
+  };
+})(jQuery, Drupal, once);
